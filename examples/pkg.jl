e3fa6e72475b0ff8e0f5bc3568efda0e684422a7
jake bolewski <jakebolewski@gmail.com>
2014-11-28 15:39:11 -0500

    add optimal pkg install example

---
+++ examples/pkg.jl
@@ -0,0 +1,50 @@
+# Optimal package install / uninstall
+# http://www.cs.ucsd.edu/~lerner/papers/opium.pdf
+
+import PicoSAT
+
+index = {
+    "a" => {:depends => ["b", "c", "z"]},
+    "b" => {:depends => ["d"]},
+    "c" => {:depends => ["d|e", "f|g"]},
+    "d" => {:conflicts => ["e"]},
+    "e" => {:conflicts => ["d"]},
+    "f" => {:conflicts => ["g"]},
+    "g" => {:conflicts => ["f"]},
+    "y" => {:depends => ["z"]},
+    "z" => Dict{Any,Any}()
+}
+
+name2var = Dict{Any,Any}()
+var2name = Dict{Any,Any}()
+
+for (i,name) in enumerate(keys(index))
+    name2var[name] = i
+    var2name[i] = name
+end
+
+clauses = Any[]
+function tocnf(name)
+    depends = get(index[name], :depends, Any[])
+    for r in depends
+        clause = Any[-(name2var[name])]
+        append!(clause, Any[name2var[n] for n in split(r, '|')])
+        push!(clauses, clause)
+    end
+    conflicts = get(index[name], :conflicts, Any[])
+    for c in conflicts
+        push!(clauses, Any[-(name2var[name]), -(name2var[c])])
+    end
+end
+
+for name in keys(index)
+    tocnf(name)
+end
+
+println(name2var)
+push!(clauses, [name2var["a"]])
+
+for sol in PicoSAT.itersolve(clauses)
+    s = [var2name[v] for v in sol[sol .> 0]]
+    println(sort(s))
+end
